d0492033cd6a962151488a7c366d50876ee99eca
Ryan Schmukler <rschmukler@gmail.com>
2014-12-24 21:56:40 -0600

    add tests

--- test/touchp-test.js
+++ test/touchp-test.js
@@ -1,14 +1,57 @@
 var expect = require('expect');
+var exists = require('fs').existsSync;
+var writeFile = require('fs').writeFileSync;
+var rmdir = require('rimraf');
+var mkdir = require('mkdirp');
+
+var touchp = require('../');
 
 describe('touchp', function() {
 
-  describe('touchp', function() {
-    it('creates a non existing file');
-    it('does nothing if the file already exists');
+  beforeEach(function(done) {
+    mkdir('./test/fixtures', done);
   });
 
-  describe('touchp.sync', function() {
-    it('creates a non existing file');
-    it('does nothing if the file already exists');
+  afterEach(function(done) {
+    rmdir('./test/fixtures', done);
   });
+
+  it('exposes mkdir', function() {
+    expect(touchp.mkdirp).toBe(mkdir);
+  });
+
+  describe('async', function() {
+    it('creates a non existing file', function(done) {
+      touchp('./test/fixtures/someFile', function(err, alreadyExisted) {
+        expect(err).toBe(null);
+        expect(alreadyExisted).toBe(false);
+        expect(exists('./test/fixtures/someFile')).toBe(true);
+        done();
+      });
+    });
+
+    it('does nothing if the file already exists', function(done) {
+      writeFile('./test/fixtures/someFile', '');
+      touchp('./test/fixtures/someFile', function(err, alreadyExisted) {
+        expect(err).toBe(null);
+        expect(alreadyExisted).toBe(true);
+        done();
+      });
+    });
+  });
+  describe('sync', function() {
+
+    it('creates a non existing file', function() {
+      var alreadyExisted = touchp.sync('./test/fixtures/someFile');
+      expect(alreadyExisted).toBe(false);
+      expect(exists('./test/fixtures/someFile')).toBe(true);
+    });
+
+    it('does nothing if the file already exists', function() {
+      writeFile('./test/fixtures/someFile', '');
+      var alreadyExisted = touchp.sync('./test/fixtures/someFile');
+      expect(alreadyExisted).toBe(true);
+    });
+  });
+
 });
